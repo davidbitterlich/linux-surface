86aa60e7fda22033be7b5b7e0150d492c8b56e19
David Bitterlich <d.bitterlich@raynet.de>
2026-09-03 15:40:46 +0200

    Update kernel version 7.2.3-0

--- pkg/fedora/kernel-surface/build-linux-surface.py
+++ pkg/fedora/kernel-surface/build-linux-surface.py
@@ -19,7 +19,7 @@ PACKAGE_NAME = "surface"
 ## Fedora tags: kernel-X.Y.Z
 ## Upstream tags: vX.Y.Z
 ##
-PACKAGE_TAG = "kernel-7.2.2-0"
+PACKAGE_TAG = "kernel-7.2.3-0"
 
 ##
 ## The release number of the modified kernel package.
